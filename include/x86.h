sleepCOW/xv6-labs

Remove trailing white space with:  for f in *.{h,c}; do sed -i .sed 's/[[:blank:]]*$//' $f; done (Thanks to Nicolás Wolovick)

x86.h
@@ -121,7 +121,7 @@ static inline uint
 xchg(volatile uint *addr, uint newval)
 {
   uint result;
-  
+
   // The + in "+m" denotes a read-modify-write operand.
   asm volatile("lock; xchgl %0, %1" :
                "+m" (*addr), "=a" (result) :
@@ -139,7 +139,7 @@ rcr2(void)
 }
 
 static inline void
-lcr3(uint val) 
+lcr3(uint val)
 {
   asm volatile("movl %0,%%cr3" : : "r" (val));
 }

tyfkda/xv6

include/x86.h
@@ -133,7 +133,7 @@ static inline uint
 xchg(volatile uint *addr, uintp newval)
 {
   uint result;
-  
+
   // The + in "+m" denotes a read-modify-write operand.
   asm volatile("lock; xchgl %0, %1" :
                "+m" (*addr), "=a" (result) :
@@ -151,7 +151,7 @@ rcr2(void)
 }
 
 static inline void
-lcr3(uintp val) 
+lcr3(uintp val)
 {
   asm volatile("mov %0,%%cr3" : : "r" (val));
 }
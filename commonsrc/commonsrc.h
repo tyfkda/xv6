b6589be48362b0a6506e72b60841740d9d4bc0d9
tyfkda <tyfkda@gmail.com>
2018-07-30 10:11:56 +0900

    Fix string.h

--- commonsrc/commonsrc.h
+++ commonsrc/commonsrc.h
@@ -3,14 +3,15 @@
 #include "stddef.h"
 
 // string.c
-int memcmp(const void*, const void*, size_t);
+char* safestrcpy(char*, const char*, int);
+char* strncpy(char*, const char*, size_t);
+char* strchr(const char*, char);
+int strncmp(const char*, const char*, size_t);
+int strlen(const char*);
+
+int memcmp(const void *v1, const void *v2, size_t n);
+void* memcpy(void *dst, const void *src, size_t n);
 void* memmove(void*, const void*, size_t);
 void* memset(void*, int, size_t);
-char* safestrcpy(char*, const char*, int);
-int strlen(const char*);
-int strncmp(const char*, const char*, size_t);
-char* strncpy(char*, const char*, size_t);
-int strlen(const char*);
-char* strchr(const char*, char);
 
 int atoi(const char *s);
--- include/string.h
+++ include/string.h
@@ -3,10 +3,14 @@
 #include "stddef.h"
 
 char* strcpy(char*, const char*);
-void *memmove(void*, const void*, int);
+char* strncpy(char *s, const char *t, size_t n);
 char* strchr(const char*, char);
 int strcmp(const char*, const char*);
 int strncmp(const char*, const char*, size_t);
 size_t strlen(const char*);
 char* strdup(const char*);
+
+int memcmp(const void *v1, const void *v2, size_t n);
+void* memcpy(void *dst, const void *src, size_t n);
+void* memmove(void*, const void*, size_t);
 void* memset(void*, int, size_t);

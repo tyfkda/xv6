fe793656f7ebd001718862a560fff407b47b582e
tyfkda <tyfkda@gmail.com>
2018-09-06 16:50:23 +0900

    hlt挿入

--- kernel/main.rs
+++ kernel/main.rs
@@ -9,22 +9,31 @@ use core::panic::PanicInfo;
 
 //const CRTPORT: u16 = 0x03d4;
 
-//fn outb(port: u16, data: u8)
-//{
+//fn outb(port: u16, data: u8) {
 //  unsafe {
 //    asm!("out $0,$1" : : "0" (data), "1" (port));
 //  }
 //}
 
+fn hlt() {
+  unsafe {
+    asm!("hlt");
+  }
+}
+
 #[no_mangle]
 pub extern fn main() {
   //outb(CRTPORT, 14);
-  loop {}
+  loop {
+    hlt();
+  }
 }
 
 #[no_mangle]
 pub extern fn mpenter() {
-  loop {}
+  loop {
+    hlt();
+  }
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}

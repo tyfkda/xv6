6c12b13c62d3a7ad4f8b60198368b24e1aa98784
tyfkda <tyfkda@gmail.com>
2018-09-06 16:04:50 +0900

    mainの必要な処理以外を削除

--- kernel/main.c
+++ kernel/main.c
@@ -3,10 +3,6 @@
 #include "mmu.h"
 #include "x86.h"
 
-//static void startothers(void);
-//static void mpmain(void)  __attribute__((noreturn));
-extern char end[]; // first address after kernel loaded from ELF file
-
 #define CRTPORT 0x3d4
 static ushort *crt = (ushort*)P2V(0xb8000);  // CGA memory
 
@@ -16,28 +12,6 @@ static ushort *crt = (ushort*)P2V(0xb8000);  // CGA memory
 int
 main(void)
 {
-#if 0
-  uartearlyinit();
-  kinit1(end, P2V(4*1024*1024)); // phys page allocator
-  kvmalloc();      // kernel page table
-  if (acpiinit()) // try to use acpi for machine info
-    mpinit();      // otherwise use bios MP tables
-  lapicinit();     // interrupt controller
-  seginit();       // segment descriptors
-  picinit();       // disable pic
-  ioapicinit();    // another interrupt controller
-  consoleinit();   // console hardware
-  uartinit();      // serial port
-  pinit();         // process table
-  tvinit();        // trap vectors
-  binit();         // buffer cache
-  fileinit();      // file table
-  ideinit();       // disk
-  startothers();   // start other processors
-  kinit2(P2V(4*1024*1024), P2V(PHYSTOP)); // must come after startothers()
-  userinit();      // first user process
-  mpmain();        // finish this processor's setup
-#else
   // Cursor position: col + SCRW * row.
   outb(CRTPORT, 14);
   int pos = inb(CRTPORT + 1) << 8;
@@ -48,81 +22,17 @@ main(void)
 
   for (;;)
     hlt();
-#endif
 }
 
 // Other CPUs jump here from entryother.S.
 void
 mpenter(void)
 {
-#if 0
-  switchkvm();
-  seginit();
-  lapicinit();
-  mpmain();
-#else
   for (;;)
     hlt();
-#endif
 }
-
-#if 0
-// Common CPU setup code.
-static void
-mpmain(void)
-{
-  cprintf("cpu%d: starting %d\n", cpuid(), cpuid());
-  idtinit();       // load idt register
-  xchg(&(mycpu()->started), 1); // tell startothers() we're up
-  scheduler();     // start running processes
-}
-#endif
 
 extern pde_t entrypgdir[];  // For entry.S
-void entry32mp(void);
-
-#if 0
-// Start the non-boot (AP) processors.
-static void
-startothers(void)
-{
-  extern uchar _binary_out_entryother_start[], _binary_out_entryother_size[];
-  uchar *code;
-  struct cpu *c;
-  char *stack;
-
-  // Write entry code to unused memory at 0x7000.
-  // The linker has placed the image of entryother.S in
-  // _binary_entryother_start.
-  code = P2V(0x7000);
-  memmove(code, _binary_out_entryother_start, (uintp)_binary_out_entryother_size);
-
-  for(c = cpus; c < cpus+ncpu; c++){
-    if(c == mycpu())  // We've started already.
-      continue;
-
-    // Tell entryother.S what stack to use, where to enter, and what
-    // pgdir to use. We cannot use kpgdir yet, because the AP processor
-    // is running in low  memory, so we use entrypgdir for the APs too.
-    stack = kalloc();
-#if X64
-    *(uint32*)(code-4) = 0x8000; // just enough stack to get us to entry64mp
-    *(uint32*)(code-8) = V2P(entry32mp);
-    *(uint64*)(code-16) = (uint64) (stack + KSTACKSIZE);
-#else
-    *(void**)(code-4) = stack + KSTACKSIZE;
-    *(void(**)(void))(code-8) = mpenter;
-    *(int**)(code-12) = (void *) V2P(entrypgdir);
-#endif
-
-    lapicstartap(c->apicid, V2P(code));
-
-    // wait for cpu to finish mpmain()
-    while(c->started == 0)
-      ;
-  }
-}
-#endif
 
 #ifndef X64
 // The boot page table used in entry.S and entryother.S.
--- kernel/param.h
+++ kernel/param.h
@@ -1,15 +1,3 @@
 #pragma once
 
-#define NPROC        64  // maximum number of processes
 #define KSTACKSIZE 4096  // size of per-process kernel stack
-#define NCPU          8  // maximum number of CPUs
-#define NOFILE       16  // open files per process
-#define NFILE       100  // open files per system
-#define NINODE       50  // maximum number of active i-nodes
-#define NDEV         10  // maximum major device number
-#define ROOTDEV       1  // device number of file system root disk
-#define MAXARG       32  // max exec arguments
-#define MAXENV       32  // max execve environments
-#define MAXOPBLOCKS  10  // max # of blocks any FS op writes
-#define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
-#define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache

e086dbfa7f56ab4a17a310cf4f38da156e9dafb0
tyfkda <tyfkda@gmail.com>
2019-01-20 17:02:12 +0900

    Add command line option to echo not to put newline

--- user/echo.c
+++ user/echo.c
@@ -1,11 +1,29 @@
+#include "stdbool.h"
 #include "stdio.h"
+#include "string.h"
 
 int
 main(int argc, char *argv[])
 {
+  bool first, newline;
   int i;
 
-  for(i = 1; i < argc; i++)
-    printf("%s%s", argv[i], i+1 < argc ? " " : "\n");
+  newline = true;
+  for (i = 1; i < argc; ++i) {
+    if (strcmp(argv[i], "-n") == 0)
+      newline = false;
+    else
+      break;
+  }
+
+  first = true;
+  for(; i < argc; ++i) {
+    printf("%s%s", first ? "" : " ", argv[i]);
+    first = false;
+  }
+
+  if (newline)
+    printf("\n");
+
   return 0;
 }

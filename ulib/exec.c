20d5817d76d23b38d7811cafa407b8a083c840a6
tyfkda <tyfkda@gmail.com>
2019-03-03 12:06:05 +0000

    Set argv[0] to exe's path in execvp

--- ulib/exec.c
+++ ulib/exec.c
@@ -1,4 +1,5 @@
 #include "errno.h"
+#include "stdbool.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
@@ -6,113 +7,35 @@
 #include "unistd.h"
 #include "../kernel/param.h"
 
-#define PATH_ENV_VAR "PATH"
 #define ENV_VAR_LEN    (128)
-#define MAX_PATH_NR    (32)
 #define PATH_MAX       (128)
 #define PATH_SEPARATOR ':'
 #define FILE_SEPARATOR '/'
 
-#ifndef FALSE
-#define FALSE (0)
-#endif
-#ifndef TRUE
-#define TRUE  (1)
-#endif
+extern int _env_find_by_idx(int _idx, char **_namep, char **_valp);
 
-typedef struct _path_store{
-  char *path_str;
-  char *path[MAX_PATH_NR];  // Note: path[MAX_PATH_NR - 1] should be '\0'
-}path_store;
+static bool find_path(const char* cmd, char* out) {
+  char *envpath = getenv("PATH");
+  if (envpath == NULL)
+    return false;
 
-int _env_find_by_idx(int _idx, char **_namep, char **_valp);
-
-static path_store g_path={0, {0}};
-
-static char*
-skip_path_delim(char *val, char **pathp){
-  char *s;
-  int len;
-
-  while (*val == PATH_SEPARATOR)
-    ++val;
-  if (*val == '\0')
-    return 0;
-
-  s = val;
-  while (*val != PATH_SEPARATOR && *val != '\0')
-    ++val;
-
-  len = val - s;
-  if (len >= PATH_MAX)
-    len = PATH_MAX - 1;
-  s[len] = '\0';
-
-  *pathp = s;
-
-  ++val;
-  while (*val == PATH_SEPARATOR)
-    ++val;
-
-  return val;
-}
-
-const char *
-path_refer(int idx) {
-  if (idx < 0 || idx >= MAX_PATH_NR)
-    return 0;
-  return g_path.path[idx];
-}
-
-int
-_path_update(const char *path_val) {
-  int i, rc;
-  char *new_path;
-  char *name, *next;
-
-  new_path = strdup(path_val);
-  if (new_path == NULL) {
-    rc = ENOMEM;
-    goto free_out;
-  }
-
-  if (g_path.path_str != NULL)
-    free(g_path.path_str);
-  for (i = 0; i < MAX_PATH_NR; ++i)
-    g_path.path[i] = NULL;
-  g_path.path_str = new_path;
-
-  i = 0;
-  next = skip_path_delim(new_path, &name);
-  while (next != NULL && *name != '\0') {
-    g_path.path[i] = name;
-    next = skip_path_delim(next, &name);
-    ++i;
-    if (i == MAX_PATH_NR - 1)
-      break;
-  }
-  g_path.path[i] = NULL;
-
-  return 0;
-
-free_out:
-  free(new_path);
-  return rc;
-}
-
-static int findExe(const char* path, char* out) {
-  char *envpath = getenv(PATH_ENV_VAR);
-  if (envpath != NULL)
-    _path_update(envpath);
-
-  const char *dir;
-  for (int i = 0; (dir = path_refer(i)) != NULL; ++i) {
-    snprintf(out, PATH_MAX, "%s/%s", dir, path);
+  for (char *p = envpath; *p != '\0'; ) {
+    char *sep = strchr(p, PATH_SEPARATOR);
+    if (sep != NULL) {
+      int len = sep - p;
+      strncpy(out, p, len + 1);
+      out[len] = FILE_SEPARATOR;
+      strncpy(&out[len + 1], cmd, PATH_MAX - len - 1);
+      p = sep + 1;
+    } else {
+      snprintf(out, PATH_MAX, "%s%c%s", p, FILE_SEPARATOR, cmd);
+      p += strlen(p);  // end
+    }
     struct stat st;
     if (stat(out, &st) == 0)
-      return TRUE;
+      return true;
   }
-  return FALSE;
+  return false;
 }
 
 int
@@ -131,6 +54,7 @@ execv(const char *path, char *const argv[]){
     envs[i] = strdup(estr);
     if (envs[i] == NULL) {
       rc = -1;
+      errno = ENOMEM;
       goto free_env_out;
     }
   }
@@ -156,7 +80,12 @@ execvp(const char *path, char *const argv[]){
     return execv(path, argv);
 
   char exepath[PATH_MAX];
-  if (!findExe(path, exepath))
+  if (!find_path(path, exepath)) {
+    errno = ENOENT;
     return -1;
-  return execv(exepath, argv);
+  }
+  char *newargv[MAXARG];
+  memcpy(newargv, argv, sizeof(newargv));
+  newargv[0] = exepath;
+  return execv(exepath, newargv);
 }
